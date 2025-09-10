112ffd0a4713b46a96a744c0ece9f42d157148cf
Diongashi09 <dion.gashi2@student.uni-pr.edu>
2025-09-10 22:33:12 +0200

    authenthication and autherization feature

---
+++ database/migrations/2025_09_10_192606_create_permissions_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('permissions', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();            // e.g. "Create Users"
+            $table->enum('verb', ['create','read','update','delete']);
+            $table->string('resource');      
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('permissions');
+    }
+};
